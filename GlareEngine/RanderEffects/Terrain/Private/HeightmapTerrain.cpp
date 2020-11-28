2bb2e046245e6fb76b98abd2b796eb9014296007
LihbGame <2252895330@qq.com>
2020-11-28 20:44:46 +0800

    update terrain

--- GlareEngine/RanderEffects/Terrain/Private/HeightmapTerrain.cpp
+++ GlareEngine/RanderEffects/Terrain/Private/HeightmapTerrain.cpp
@@ -89,20 +89,89 @@ void HeightmapTerrain::LoadHeightmapAsset()
 	{
 		TerrainTextures[e].push_back(pTextureManage->GetTexture(wstring(e.begin(), e.end())).get());
 	}
+
+
+	// A height for each vertex
+	std::vector<unsigned char> in(mInfo.HeightmapWidth * mInfo.HeightmapHeight);
+
+	// Open the file.
+	std::ifstream inFile;
+	inFile.open(mInfo.HeightMapFilename.c_str(), std::ios_base::binary);
+
+	if (inFile)
+	{
+		// Read the RAW bytes.
+		inFile.read((char*)&in[0], (std::streamsize)in.size());
+		// Done with file.
+		inFile.close();
+	}
+
+	// Copy the array data into a float array and scale it.
+	mHeightmap.resize(mInfo.HeightmapHeight * mInfo.HeightmapWidth, 0);
+	for (UINT i = 0; i < mInfo.HeightmapHeight * mInfo.HeightmapWidth; ++i)
+	{
+		mHeightmap[i] = (in[i] / 255.0f) * mInfo.HeightScale;
+	}
+
 }
 
 void HeightmapTerrain::Smooth()
 {
+	std::vector<float> dest(mHeightmap.size());
+
+	for (UINT i = 0; i < mInfo.HeightmapHeight; ++i)
+	{
+		for (UINT j = 0; j < mInfo.HeightmapWidth; ++j)
+		{
+			dest[i * mInfo.HeightmapWidth + j] = Average(i, j);
+		}
+	}
+
+	// Replace the old height map with the filtered one.
+	mHeightmap = dest;
 }
 
 bool HeightmapTerrain::InBounds(int i, int j)
 {
-	return false;
+	// True if ij are valid indices; false otherwise.
+	return
+		i >= 0 && i < (int)mInfo.HeightmapHeight&&
+		j >= 0 && j < (int)mInfo.HeightmapWidth;
 }
 
 float HeightmapTerrain::Average(int i, int j)
 {
-	return 0.0f;
+	// Function computes the average height of the ij element.
+	// It averages itself with its eight neighbor pixels.  Note
+	// that if a pixel is missing neighbor, we just don't include it
+	// in the average--that is, edge pixels don't have a neighbor pixel.
+	//
+	// ----------
+	// | 1| 2| 3|
+	// ----------
+	// |4 |ij| 6|
+	// ----------
+	// | 7| 8| 9|
+	// ----------
+
+	float avg = 0.0f;
+	float num = 0.0f;
+
+	// Use int to allow negatives.  If we use UINT, @ i=0, m=i-1=UINT_MAX
+	// and no iterations of the outer for loop occur.
+	for (int m = i - 1; m <= i + 1; ++m)
+	{
+		for (int n = j - 1; n <= j + 1; ++n)
+		{
+			if (InBounds(m, n))
+			{
+				avg += mHeightmap[m * mInfo.HeightmapWidth + n];
+				num += 1.0f;
+			}
+		}
+	}
+
+	return avg / num;
 }
 
 void HeightmapTerrain::CalcAllPatchBoundsY()
