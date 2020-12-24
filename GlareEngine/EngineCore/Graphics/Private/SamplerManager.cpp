9d13ab1b2fa9ccc99269b5f74102db46959911f4
LihbGame <2252895330@qq.com>
2020-12-24 23:16:22 +0800

    add graphics core

---
+++ GlareEngine/EngineCore/Graphics/Private/SamplerManager.cpp
@@ -0,0 +1,26 @@
+#include "SamplerManager.h"
+
+namespace
+{
+	map< size_t, D3D12_CPU_DESCRIPTOR_HANDLE > s_SamplerCache;
+}
+
+
+D3D12_CPU_DESCRIPTOR_HANDLE SamplerDesc::CreateDescriptor()
+{
+	size_t hashValue = Utility::HashState(this);
+	auto iter = s_SamplerCache.find(hashValue);
+	if (iter != s_SamplerCache.end())
+	{
+		return iter->second;
+	}
+
+	D3D12_CPU_DESCRIPTOR_HANDLE Handle = AllocateDescriptor(D3D12_DESCRIPTOR_HEAP_TYPE_SAMPLER);
+	g_Device->CreateSampler(this, Handle);
+	return Handle;
+}
+
+void SamplerDesc::CreateDescriptor(D3D12_CPU_DESCRIPTOR_HANDLE& Handle)
+{
+	g_Device->CreateSampler(this, Handle);
+}
---
+++ GlareEngine/EngineCore/Graphics/Public/GraphicsCore.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "L3DUtil.h"
+
+
+namespace Graphics
+{
+#ifndef RELEASE
+	extern const GUID WKPDID_D3DDebugObjectName;
+#endif
+	using namespace Microsoft::WRL;
+
+	void Initialize(void);
+	void Resize(uint32_t width, uint32_t height);
+	void Terminate(void);
+	void Shutdown(void);
+	void Present(void);
+
+	extern uint32_t g_DisplayWidth;
+	extern uint32_t g_DisplayHeight;
+
+	// Returns the number of elapsed frames since application start
+	uint64_t GetFrameCount(void);
+
+	// The amount of time elapsed during the last completed frame.  The CPU and/or
+	// GPU may be idle during parts of the frame.  The frame time measures the time
+	// between calls to present each frame.
+	float GetFrameTime(void);
+
+	// The total number of frames per second
+	float GetFrameRate(void);
+
+	extern ID3D12Device* g_Device;
+	extern CommandListManager g_CommandManager;
+	extern ContextManager g_ContextManager;
+
+	extern D3D_FEATURE_LEVEL g_D3DFeatureLevel;
+	extern bool g_bTypedUAVLoadSupport_R11G11B10_FLOAT;
+	extern bool g_bEnableHDROutput;
+
+}
