574566b7282ed37477fa78eef64f3a3b15dd1bcc
LihbGame <2252895330@qq.com>
2021-01-15 19:34:19 +0800

    upadte water shader

--- GlareEngine/Shaders/ShockWaveWaterPS.hlsl
+++ GlareEngine/Shaders/ShockWaveWaterPS.hlsl
@@ -50,8 +50,8 @@ float4 PS(VertexOut pin) : SV_Target
     float3 vBumpTex = normalize(2.0 * (vBumpTexA.xyz + vBumpTexB.xyz + vBumpTexC.xyz + vBumpTexD.xyz) - 4.0);
 
     // Apply individual bump scale for refraction and reflection
-    float3 vRefrBump = vBumpTex.xyz * float3(0.002, 0.002, 1.0);
-    float3 vReflBump = vBumpTex.xyz * float3(0.01, 0.01, 1.0);
+    float3 vRefrBump = vBumpTex.xyz * float3(0.001, 0.005, 1.0);
+    float3 vReflBump = vBumpTex.xyz * float3(0.02, 0.02, 1.0);
 
 
     // Compute projected coordinates
@@ -63,10 +63,12 @@ float4 PS(VertexOut pin) : SV_Target
     // Mask occluders from refraction map
     float4 vRefraction = vRefrB * vRefrA.w + vRefrA * (1 - vRefrA.w);
 
+
+
     // Compute Fresnel term
     float NdotL = max(dot(vEye, vReflBump), 0);
-    float facing = (1.0 - NdotL);
-    float fresnel = Fresnel(NdotL, 0.02, 5.0);
+    float facing = (1 - NdotL);
+    float fresnel = Fresnel(NdotL, 0.01, 5.0);
 
     //Foam and heigh to water
     half Heigh = gSRVMap[mHeightMapIndex].SampleLevel(gsamLinearWrap, pin.HeighTex.xy, 0).r;
@@ -75,21 +77,21 @@ float4 PS(VertexOut pin) : SV_Target
     float fDistScale = saturate(50 / pin.Wave0.w);
     float3 WaterDeepColor = (vRefraction.xyz * fDistScale + (1 - fDistScale) * float3(0.0025, 0.1, 0.125));
     // Lerp between water color and deep water color
-    float3 WaterColor = float3(0.005, 0.1, 0.15);
-    float3 waterColor = (WaterColor * facing + WaterDeepColor * (1.0 - facing));
-    float3 cReflect = fresnel*  vReflection.xyz*0.5f;//Reflection Scale=0.5f
+    float3 WaterColor = float3(0.05, 0.1, 0.15);
+    float3 waterColor = WaterDeepColor;// (WaterColor* facing + WaterDeepColor * (1.0 - facing));
+    float3 cReflect = fresnel * vReflection.xyz * 0.4f;//Reflection Scale=0.5f
 
 
 
     float3 Foam = float3(0.0f, 0.0f, 0.0f);
-    float time = abs(cos(gTotalTime/3));
-    if (-Heigh >= time && -Heigh <= time + 1.0f)
+    float time = abs(cos(gTotalTime / 3));
+    if (-Heigh >= time && -Heigh <= time + 3.0f)
     {
         Foam.rgb = float3(1.0f, 1.0f, 1.0f);// gFoamMap.Sample(samLinear, pin.HeighTex.xy).rgb;
         //float timewave = time*Heigh;
-        Foam *= (1-time) * 0.3f;
+        Foam *= (1 - time) * 0.3f + 0.01f;
     }
-    if (-Heigh < time||Heigh>time + 1.0f)
+    if (-Heigh < time || Heigh>time)
     {
       clip(-1.0f);
     }
@@ -108,17 +110,17 @@ float4 PS(VertexOut pin) : SV_Target
 
 
      texColor = ComputeLighting(gLights, mat, pin.PosW,
-        bumpedNormalW, normalize(gEyePosW.xyz - pin.PosW), shadowFactor)+ texColor;
+        bumpedNormalW, normalize(gEyePosW.xyz - pin.PosW), shadowFactor) + texColor;
 
-    //
-    // Fogging
-    //
-    if (gFogEnabled)
-    {
-    float fogLerp = saturate((length(gEyePosW - pin.PosW) - gFogStart) / gFogRange);
-    // Blend the fog color and the lit color.
-    texColor.rgb = lerp(texColor.rgb, gFogColor.rgb, fogLerp);
-    }
+     //
+     // Fogging
+     //
+     if (gFogEnabled)
+     {
+     float fogLerp = saturate((length(gEyePosW - pin.PosW) - gFogStart) / gFogRange);
+     // Blend the fog color and the lit color.
+     texColor.rgb = lerp(texColor.rgb, gFogColor.rgb, fogLerp);
+     }
 
-    return texColor;
+     return texColor;
 }
