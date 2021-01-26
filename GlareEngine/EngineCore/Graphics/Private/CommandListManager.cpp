7b0f8550b517328cc5477b5c73bc0bb889d0a31e
LihbGame <2252895330@qq.com>
2021-01-26 22:46:34 +0800

    update CommandlistManager

--- GlareEngine/EngineCore/Graphics/Private/CommandListManager.cpp
+++ GlareEngine/EngineCore/Graphics/Private/CommandListManager.cpp
@@ -119,3 +119,57 @@ void GlareEngine::DirectX12Graphics::CommandQueue::DiscardAllocator(uint64_t Fen
 {
 	m_AllocatorPool.DiscardAllocator(FenceValueForReset, Allocator);
 }
+
+
+GlareEngine::DirectX12Graphics::CommandListManager::CommandListManager()
+:m_Device(nullptr),
+m_GraphicsQueue(D3D12_COMMAND_LIST_TYPE_DIRECT),
+m_ComputeQueue(D3D12_COMMAND_LIST_TYPE_COMPUTE),
+m_CopyQueue(D3D12_COMMAND_LIST_TYPE_COPY)
+{
+}
+
+GlareEngine::DirectX12Graphics::CommandListManager::~CommandListManager()
+{
+	Shutdown();
+}
+
+void GlareEngine::DirectX12Graphics::CommandListManager::Create(ID3D12Device* pDevice)
+{
+	assert(pDevice != nullptr);
+
+	m_Device = pDevice;
+
+	m_GraphicsQueue.Create(pDevice);
+	m_ComputeQueue.Create(pDevice);
+	m_CopyQueue.Create(pDevice);
+}
+
+void GlareEngine::DirectX12Graphics::CommandListManager::Shutdown()
+{
+	m_GraphicsQueue.Shutdown();
+	m_ComputeQueue.Shutdown();
+	m_CopyQueue.Shutdown();
+}
+
+void GlareEngine::DirectX12Graphics::CommandListManager::CreateNewCommandList(D3D12_COMMAND_LIST_TYPE Type, ID3D12GraphicsCommandList** List, ID3D12CommandAllocator** Allocator)
+{
+	assert(Type != D3D12_COMMAND_LIST_TYPE_BUNDLE, "Bundles are not yet supported");
+	switch (Type)
+	{
+	case D3D12_COMMAND_LIST_TYPE_DIRECT: *Allocator = m_GraphicsQueue.RequestAllocator(); break;
+	case D3D12_COMMAND_LIST_TYPE_BUNDLE: break;
+	case D3D12_COMMAND_LIST_TYPE_COMPUTE: *Allocator = m_ComputeQueue.RequestAllocator(); break;
+	case D3D12_COMMAND_LIST_TYPE_COPY: *Allocator = m_CopyQueue.RequestAllocator(); break;
+	}
+
+	ThrowIfFailed(m_Device->CreateCommandList(1, Type, *Allocator, nullptr, IID_PPV_ARGS(List)));
+	(*List)->SetName(L"CommandList");
+}
+
+
+void GlareEngine::DirectX12Graphics::CommandListManager::WaitForFence(uint64_t FenceValue)
+{
+	CommandQueue& Producer = DirectX12Graphics::g_CommandManager.GetQueue((D3D12_COMMAND_LIST_TYPE)(FenceValue >> 56));
+	Producer.WaitForFence(FenceValue);
+}
