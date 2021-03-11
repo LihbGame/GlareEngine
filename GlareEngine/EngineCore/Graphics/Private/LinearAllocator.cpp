de8ca55e4ff11cc850d2ac543c945418339136b8
LihbGame <2252895330@qq.com>
2021-03-11 20:19:15 +0800

    finish linearAllocator

--- GlareEngine/EngineCore/Graphics/Private/LinearAllocator.cpp
+++ GlareEngine/EngineCore/Graphics/Private/LinearAllocator.cpp
@@ -111,3 +111,68 @@ LinearAllocationPage* LinearAllocatorPageManager::CreateNewPage(size_t PageSize)
 
 	return new LinearAllocationPage(pBuffer, DefaultUsage);
 }
+
+void LinearAllocator::CleanupUsedPages(uint64_t FenceID)
+{
+	if (m_CurPage == nullptr)
+		return;
+
+	m_RetiredPages.push_back(m_CurPage);
+	m_CurPage = nullptr;
+	m_CurOffset = 0;
+
+	sm_PageManager[m_AllocationType].DiscardPages(FenceID, m_RetiredPages);
+	m_RetiredPages.clear();
+
+	sm_PageManager[m_AllocationType].FreeLargePages(FenceID, m_LargePageList);
+	m_LargePageList.clear();
+}
+
+DynamicAlloc LinearAllocator::AllocateLargePage(size_t SizeInBytes)
+{
+	LinearAllocationPage* OneOff = sm_PageManager[m_AllocationType].CreateNewPage(SizeInBytes);
+	m_LargePageList.push_back(OneOff);
+
+	DynamicAlloc ret(*OneOff, 0, SizeInBytes);
+	ret.DataPtr = OneOff->m_CPUVirtualAddress;
+	ret.GPUAddress = OneOff->m_GPUVirtualAddress;
+
+	return ret;
+}
+
+DynamicAlloc LinearAllocator::Allocate(size_t SizeInBytes, size_t Alignment)
+{
+	const size_t AlignmentMask = Alignment - 1;
+
+	// Assert that it's a power of two.
+	assert((AlignmentMask & Alignment) == 0);
+
+	// Align the allocation
+	const size_t AlignedSize = Math::AlignUpWithMask(SizeInBytes, AlignmentMask);
+
+	if (AlignedSize > m_PageSize)
+		return AllocateLargePage(AlignedSize);
+
+	m_CurOffset = Math::AlignUp(m_CurOffset, Alignment);
+
+	if (m_CurOffset + AlignedSize > m_PageSize)
+	{
+		assert(m_CurPage != nullptr);
+		m_RetiredPages.push_back(m_CurPage);
+		m_CurPage = nullptr;
+	}
+
+	if (m_CurPage == nullptr)
+	{
+		m_CurPage = sm_PageManager[m_AllocationType].RequestPage();
+		m_CurOffset = 0;
+	}
+
+	DynamicAlloc ret(*m_CurPage, m_CurOffset, AlignedSize);
+	ret.DataPtr = (uint8_t*)m_CurPage->m_CPUVirtualAddress + m_CurOffset;
+	ret.GPUAddress = m_CurPage->m_GPUVirtualAddress + m_CurOffset;
+
+	m_CurOffset += AlignedSize;
+
+	return ret;
+}
