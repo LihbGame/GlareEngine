2736a8da0643adea9585e4219e175247fb3e740a
LihbGame <2252895330@qq.com>
2021-05-09 00:21:43 +0800

    update engine input

--- GlareEngine/EngineCore/Engine/Private/EngineInput.cpp
+++ GlareEngine/EngineCore/Engine/Private/EngineInput.cpp
@@ -1,9 +1,279 @@
 #include "EngineInput.h"
+#include <cassert>
 
+#define USE_XINPUT
+#include <XInput.h>
+#pragma comment(lib, "xinput9_1_0.lib")
 
-void GlareEngine::EngineInput::Initialize()
+#define USE_KEYBOARD_MOUSE
+#define DIRECTINPUT_VERSION 0x0800
+#include <dinput.h>
+#pragma comment(lib, "dinput8.lib")
+#pragma comment(lib, "dxguid.lib")
+
+using namespace GlareEngine;
+
+namespace GameCore
 {
+	extern HWND g_hWnd;
+}
+
+
+namespace
+{
+	bool s_Buttons[2][EngineInput::kNumDigitalInputs];
+	float s_HoldDuration[EngineInput::kNumDigitalInputs] = { 0.0f };
+	float s_Analogs[EngineInput::kNumAnalogInputs];
+	float s_AnalogsTC[EngineInput::kNumAnalogInputs];
+
+#ifdef USE_KEYBOARD_MOUSE
+	IDirectInput8A* s_DI;
+	IDirectInputDevice8A* s_Keyboard;
+	IDirectInputDevice8A* s_Mouse;
+#endif
+
+
+	DIMOUSESTATE2 s_MouseState;
+	unsigned char s_Keybuffer[256];
+	unsigned char s_DXKeyMapping[EngineInput::kNumKeys]; // map DigitalInput enum to DX key codes 
+
+
+#ifdef USE_XINPUT
+	float FilterAnalogInput(int val, int deadZone)
+	{
+		if (val < 0)
+		{
+			if (val > -deadZone)
+				return 0.0f;
+			else
+				return (val + deadZone) / (32768.0f - deadZone);
+		}
+		else
+		{
+			if (val < deadZone)
+				return 0.0f;
+			else
+				return (val - deadZone) / (32767.0f - deadZone);
+		}
+	}
+#endif
+
+#ifdef USE_KEYBOARD_MOUSE
+	void BuildKeyMapping()
+	{
+		s_DXKeyMapping[EngineInput::kKey_Escape] = 1;
+		s_DXKeyMapping[EngineInput::kKey_1] = 2;
+		s_DXKeyMapping[EngineInput::kKey_2] = 3;
+		s_DXKeyMapping[EngineInput::kKey_3] = 4;
+		s_DXKeyMapping[EngineInput::kKey_4] = 5;
+		s_DXKeyMapping[EngineInput::kKey_5] = 6;
+		s_DXKeyMapping[EngineInput::kKey_6] = 7;
+		s_DXKeyMapping[EngineInput::kKey_7] = 8;
+		s_DXKeyMapping[EngineInput::kKey_8] = 9;
+		s_DXKeyMapping[EngineInput::kKey_9] = 10;
+		s_DXKeyMapping[EngineInput::kKey_0] = 11;
+		s_DXKeyMapping[EngineInput::kKey_Minus] = 12;
+		s_DXKeyMapping[EngineInput::kKey_Equals] = 13;
+		s_DXKeyMapping[EngineInput::kKey_Back] = 14;
+		s_DXKeyMapping[EngineInput::kKey_Tab] = 15;
+		s_DXKeyMapping[EngineInput::kKey_Q] = 16;
+		s_DXKeyMapping[EngineInput::kKey_W] = 17;
+		s_DXKeyMapping[EngineInput::kKey_E] = 18;
+		s_DXKeyMapping[EngineInput::kKey_R] = 19;
+		s_DXKeyMapping[EngineInput::kKey_T] = 20;
+		s_DXKeyMapping[EngineInput::kKey_Y] = 21;
+		s_DXKeyMapping[EngineInput::kKey_U] = 22;
+		s_DXKeyMapping[EngineInput::kKey_I] = 23;
+		s_DXKeyMapping[EngineInput::kKey_O] = 24;
+		s_DXKeyMapping[EngineInput::kKey_P] = 25;
+		s_DXKeyMapping[EngineInput::kKey_Lbracket] = 26;
+		s_DXKeyMapping[EngineInput::kKey_Rbracket] = 27;
+		s_DXKeyMapping[EngineInput::kKey_Return] = 28;
+		s_DXKeyMapping[EngineInput::kKey_Lcontrol] = 29;
+		s_DXKeyMapping[EngineInput::kKey_A] = 30;
+		s_DXKeyMapping[EngineInput::kKey_S] = 31;
+		s_DXKeyMapping[EngineInput::kKey_D] = 32;
+		s_DXKeyMapping[EngineInput::kKey_F] = 33;
+		s_DXKeyMapping[EngineInput::kKey_G] = 34;
+		s_DXKeyMapping[EngineInput::kKey_H] = 35;
+		s_DXKeyMapping[EngineInput::kKey_J] = 36;
+		s_DXKeyMapping[EngineInput::kKey_K] = 37;
+		s_DXKeyMapping[EngineInput::kKey_L] = 38;
+		s_DXKeyMapping[EngineInput::kKey_Semicolon] = 39;
+		s_DXKeyMapping[EngineInput::kKey_Apostrophe] = 40;
+		s_DXKeyMapping[EngineInput::kKey_Grave] = 41;
+		s_DXKeyMapping[EngineInput::kKey_Lshift] = 42;
+		s_DXKeyMapping[EngineInput::kKey_Backslash] = 43;
+		s_DXKeyMapping[EngineInput::kKey_Z] = 44;
+		s_DXKeyMapping[EngineInput::kKey_X] = 45;
+		s_DXKeyMapping[EngineInput::kKey_C] = 46;
+		s_DXKeyMapping[EngineInput::kKey_V] = 47;
+		s_DXKeyMapping[EngineInput::kKey_B] = 48;
+		s_DXKeyMapping[EngineInput::kKey_N] = 49;
+		s_DXKeyMapping[EngineInput::kKey_M] = 50;
+		s_DXKeyMapping[EngineInput::kKey_Comma] = 51;
+		s_DXKeyMapping[EngineInput::kKey_Period] = 52;
+		s_DXKeyMapping[EngineInput::kKey_Slash] = 53;
+		s_DXKeyMapping[EngineInput::kKey_Rshift] = 54;
+		s_DXKeyMapping[EngineInput::kKey_Multiply] = 55;
+		s_DXKeyMapping[EngineInput::kKey_Lalt] = 56;
+		s_DXKeyMapping[EngineInput::kKey_Space] = 57;
+		s_DXKeyMapping[EngineInput::kKey_Capital] = 58;
+		s_DXKeyMapping[EngineInput::kKey_F1] = 59;
+		s_DXKeyMapping[EngineInput::kKey_F2] = 60;
+		s_DXKeyMapping[EngineInput::kKey_F3] = 61;
+		s_DXKeyMapping[EngineInput::kKey_F4] = 62;
+		s_DXKeyMapping[EngineInput::kKey_F5] = 63;
+		s_DXKeyMapping[EngineInput::kKey_F6] = 64;
+		s_DXKeyMapping[EngineInput::kKey_F7] = 65;
+		s_DXKeyMapping[EngineInput::kKey_F8] = 66;
+		s_DXKeyMapping[EngineInput::kKey_F9] = 67;
+		s_DXKeyMapping[EngineInput::kKey_F10] = 68;
+		s_DXKeyMapping[EngineInput::kKey_Numlock] = 69;
+		s_DXKeyMapping[EngineInput::kKey_Scroll] = 70;
+		s_DXKeyMapping[EngineInput::kKey_Numpad7] = 71;
+		s_DXKeyMapping[EngineInput::kKey_Numpad8] = 72;
+		s_DXKeyMapping[EngineInput::kKey_Numpad9] = 73;
+		s_DXKeyMapping[EngineInput::kKey_Subtract] = 74;
+		s_DXKeyMapping[EngineInput::kKey_Numpad4] = 75;
+		s_DXKeyMapping[EngineInput::kKey_Numpad5] = 76;
+		s_DXKeyMapping[EngineInput::kKey_Numpad6] = 77;
+		s_DXKeyMapping[EngineInput::kKey_Add] = 78;
+		s_DXKeyMapping[EngineInput::kKey_Numpad1] = 79;
+		s_DXKeyMapping[EngineInput::kKey_Numpad2] = 80;
+		s_DXKeyMapping[EngineInput::kKey_Numpad3] = 81;
+		s_DXKeyMapping[EngineInput::kKey_Numpad0] = 82;
+		s_DXKeyMapping[EngineInput::kKey_Decimal] = 83;
+		s_DXKeyMapping[EngineInput::kKey_F11] = 87;
+		s_DXKeyMapping[EngineInput::kKey_F12] = 88;
+		s_DXKeyMapping[EngineInput::kKey_Numpadenter] = 156;
+		s_DXKeyMapping[EngineInput::kKey_Rcontrol] = 157;
+		s_DXKeyMapping[EngineInput::kKey_Divide] = 181;
+		s_DXKeyMapping[EngineInput::kKey_Sysrq] = 183;
+		s_DXKeyMapping[EngineInput::kKey_Ralt] = 184;
+		s_DXKeyMapping[EngineInput::kKey_Pause] = 197;
+		s_DXKeyMapping[EngineInput::kKey_Home] = 199;
+		s_DXKeyMapping[EngineInput::kKey_Up] = 200;
+		s_DXKeyMapping[EngineInput::kKey_Pgup] = 201;
+		s_DXKeyMapping[EngineInput::kKey_Left] = 203;
+		s_DXKeyMapping[EngineInput::kKey_Right] = 205;
+		s_DXKeyMapping[EngineInput::kKey_End] = 207;
+		s_DXKeyMapping[EngineInput::kKey_Down] = 208;
+		s_DXKeyMapping[EngineInput::kKey_Pgdn] = 209;
+		s_DXKeyMapping[EngineInput::kKey_Insert] = 210;
+		s_DXKeyMapping[EngineInput::kKey_Delete] = 211;
+		s_DXKeyMapping[EngineInput::kKey_Lwin] = 219;
+		s_DXKeyMapping[EngineInput::kKey_Rwin] = 220;
+		s_DXKeyMapping[EngineInput::kKey_Apps] = 221;
+	}
+
+	void ZeroInputs()
+	{
+		memset(&s_MouseState, 0, sizeof(DIMOUSESTATE2));
+		memset(s_Keybuffer, 0, sizeof(s_Keybuffer));
+	}
+
+	void KMInitialize()
+	{
+		BuildKeyMapping();
+
+#if WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_DESKTOP)
+		if (FAILED(DirectInput8Create(GetModuleHandle(nullptr), DIRECTINPUT_VERSION, IID_IDirectInput8, (void**)&s_DI, nullptr)))
+			assert(false, "DirectInput8 initialization failed.");
+		if (FAILED(s_DI->CreateDevice(GUID_SysKeyboard, &s_Keyboard, nullptr)))
+			assert(false, "Keyboard CreateDevice failed.");
+		if (FAILED(s_Keyboard->SetDataFormat(&c_dfDIKeyboard)))
+			assert(false, "Keyboard SetDataFormat failed.");
+		if (FAILED(s_Keyboard->SetCooperativeLevel(GameCore::g_hWnd, DISCL_FOREGROUND | DISCL_NONEXCLUSIVE)))
+			assert(false, "Keyboard SetCooperativeLevel failed.");
+
+		DIPROPDWORD dipdw;
+		dipdw.diph.dwSize = sizeof(DIPROPDWORD);
+		dipdw.diph.dwHeaderSize = sizeof(DIPROPHEADER);
+		dipdw.diph.dwObj = 0;
+		dipdw.diph.dwHow = DIPH_DEVICE;
+		dipdw.dwData = 10;
+		if (FAILED(s_Keyboard->SetProperty(DIPROP_BUFFERSIZE, &dipdw.diph)))
+			assert(false, "Keyboard set buffer size failed.");
+
+		if (FAILED(s_DI->CreateDevice(GUID_SysMouse, &s_Mouse, nullptr)))
+			assert(false, "Mouse CreateDevice failed.");
+		if (FAILED(s_Mouse->SetDataFormat(&c_dfDIMouse2)))
+			assert(false, "Mouse SetDataFormat failed.");
+		if (FAILED(s_Mouse->SetCooperativeLevel(GameCore::g_hWnd, DISCL_FOREGROUND | DISCL_EXCLUSIVE)))
+			assert(false, "Mouse SetCooperativeLevel failed.");
+#endif
+		ZeroInputs();
+	}
+
+	void KMShutdown()
+	{
+#if WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_DESKTOP)
+		if (s_Keyboard)
+		{
+			s_Keyboard->Unacquire();
+			s_Keyboard->Release();
+			s_Keyboard = nullptr;
+		}
+		if (s_Mouse)
+		{
+			s_Mouse->Unacquire();
+			s_Mouse->Release();
+			s_Mouse = nullptr;
+		}
+		if (s_DI)
+		{
+			s_DI->Release();
+			s_DI = nullptr;
+		}
+#endif
+	}
+
+	void KMUpdate()
+	{
+#if WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_DESKTOP)
+		HWND foreground = GetForegroundWindow();
+		bool visible = IsWindowVisible(foreground) != 0;
+
+		if (foreground != GameCore::g_hWnd // wouldn't be able to acquire
+			|| !visible)
+		{
+			ZeroInputs();
+		}
+		else
+		{
+			s_Mouse->Acquire();
+			s_Mouse->GetDeviceState(sizeof(DIMOUSESTATE2), &s_MouseState);
+			s_Keyboard->Acquire();
+			s_Keyboard->GetDeviceState(sizeof(s_Keybuffer), s_Keybuffer);
+		}
+#endif
+	}
+
+
+#endif
 
 
 
 }
+
+void EngineInput::Initialize()
+{
+	ZeroMemory(s_Buttons, sizeof(s_Buttons));
+	ZeroMemory(s_Analogs, sizeof(s_Analogs));
+
+#ifdef USE_KEYBOARD_MOUSE
+	KMInitialize();
+#endif
+}
+
+void EngineInput::Shutdown()
+{
+#ifdef USE_KEYBOARD_MOUSE
+	KMShutdown();
+#endif
+}
+
+
+
+
