e0a8f8e9313a6ce5c3ac3f51891faa2b78fa0330
LihbGame <2252895330@qq.com>
2021-06-03 00:21:52 +0800

    UPDATE  GAME CORE

--- GlareEngine/EngineCore/Engine/Private/GameCore.cpp
+++ GlareEngine/EngineCore/Engine/Private/GameCore.cpp
@@ -4,7 +4,61 @@
 #include "EngineInput.h"
 #include "BufferManager.h"
 #include "CommandContext.h"
+#include "EngineAdjust.h"
+#include "EngineProfiling.h"
 
 #pragma comment(lib, "runtimeobject.lib")
 
-using namespace GlareEngine;
+namespace GlareEngine
+{
+	namespace DirectX12Graphics
+	{
+		extern ColorBuffer g_GenMipsBuffer;
+	}
+}
+
+
+
+namespace GlareEngine
+{
+	namespace GameCore
+	{
+		using namespace GlareEngine::DirectX12Graphics;
+
+		void InitializeApplication(GameApp& Game)
+		{
+			DirectX12Graphics::Initialize();
+			EngineInput::Initialize();
+			EngineAdjust::Initialize();
+
+			Game.Startup();
+		}
+
+		void TerminateApplication(GameApp& Game)
+		{
+			Game.Cleanup();
+			EngineInput::Shutdown();
+		}
+
+		bool UpdateApplication(GameApp& Game)
+		{
+			EngineProfiling::Update();
+
+			float DeltaTime = DirectX12Graphics::GetFrameTime();
+
+			EngineInput::Update(DeltaTime);
+			EngineAdjust::Update(DeltaTime);
+
+			Game.Update(DeltaTime);
+			Game.RenderScene();
+			//Game.RenderUI();
+			//PostEffects::Render();
+
+			DirectX12Graphics::Present();
+
+			return !Game.IsDone();
+		}
+
+
+	}
+}
