6d510ada98ce5b082b98fa5a6417b28e17f11310
LihbGame <2252895330@qq.com>
2021-07-17 03:13:53 +0800

    Separate the core code of the rendering engine

---
+++ GameDemo/GameDemo.cpp
@@ -0,0 +1,38 @@
+﻿#include "L3DUtil.h"
+#include "GameCore.h"
+
+//lib
+#pragma comment(lib, "dxgi.lib")
+#pragma comment(lib, "comsuppw.lib")
+#pragma comment(lib, "d3dcompiler.lib")
+#pragma comment(lib, "D3D12.lib")
+#pragma comment(lib,"dxguid.lib")
+
+using namespace GlareEngine::GameCore;
+
+
+class App :public GameApp
+{
+public:
+	virtual ~App() {}
+
+	//此功能可用于初始化应用程序状态，并将在分配必要的硬件资源后运行。 
+	//不依赖于这些资源的某些状态仍应在构造函数中初始化，例如指针和标志。
+	virtual void Startup(void) {}// = 0;
+	virtual void Cleanup(void) {}// = 0;
+
+	//确定您是否要退出该应用。 默认情况下，应用程序继续运行，直到按下“ ESC”键为止。
+	//virtual bool IsDone(void);
+
+	//每帧将调用一次update方法。 状态更新和场景渲染都应使用此方法处理。
+	virtual void Update(float deltaT) {}// = 0;
+
+	//rendering pass
+	virtual void RenderScene(void) {}// = 0;
+
+	//可选的UI（覆盖）渲染阶段。 这是LDR。 缓冲区已被清除。 
+	virtual void RenderUI() {};
+};
+
+
+CREATE_APPLICATION(App);
