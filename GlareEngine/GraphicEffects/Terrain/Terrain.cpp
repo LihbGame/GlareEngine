1db88e8edd74e4f6fed246bf88115b38ef1a8838
LihbGame <2252895330@qq.com>
2022-03-31 22:26:15 +0800

    Update height map terrain

--- GlareEngine/GraphicEffects/Terrain/Terrain.cpp
+++ GlareEngine/GraphicEffects/Terrain/Terrain.cpp
@@ -1,1 +1,67 @@
 #include "Terrain.h"
+
+
+GraphicsPSO Terrain::mPSO;
+
+Terrain::Terrain()
+{
+
+}
+
+
+Terrain::~Terrain()
+{
+}
+
+void Terrain::Update(float dt)
+{
+}
+
+float Terrain::GetWidth() const
+{
+	return 0.0f;
+}
+
+float Terrain::GetDepth() const
+{
+	return 0.0f;
+}
+
+float Terrain::GetHeight(float x, float z) const
+{
+	return 0.0f;
+}
+
+void Terrain::CreateMaterials()
+{
+}
+
+void Terrain::LoadHeightMapFromFile(string filename)
+{
+}
+
+void Terrain::Smooth()
+{
+}
+
+bool Terrain::InBounds(int i, int j)
+{
+	return false;
+}
+
+float Terrain::Average(int i, int j)
+{
+	return 0.0f;
+}
+
+void Terrain::CalcAllPatchBoundsY()
+{
+}
+
+void Terrain::CalcPatchBoundsY(UINT i, UINT j)
+{
+}
+
+void Terrain::BuildQuadPatchGeometry()
+{
+}
--- GlareEngine/GraphicEffects/Terrain/Terrain.h
+++ GlareEngine/GraphicEffects/Terrain/Terrain.h
@@ -1,9 +1,23 @@
 #pragma once
 #include "RenderObject.h"
+#include "EngineUtility.h"
+#include "Camera.h"
+#include "TextureManager.h"
+#include "ModelMesh.h"
 
 
-
-
+struct TerrainInitInfo
+{
+	string HeightMapFilename;
+	string LayerMapFilename[5];
+	string BlendMapFilename;
+	UINT TerrainTitleSize;
+	UINT TerrainEachTitleSize;
+	float HeightScale;
+	UINT HeightmapWidth;
+	UINT HeightmapHeight;
+	float CellSpacing;
+};
 
 class Terrain :
     public RenderObject
@@ -11,10 +25,45 @@ class Terrain :
 public:
 	Terrain();
 	~Terrain();
+
+	void Update(float dt);
+
+	float GetWidth()const;
+	float GetDepth()const;
+	float GetHeight(float x, float z)const;
 private:
+	void CreateMaterials();
+	void LoadHeightMapFromFile(string filename);
+
+	void Smooth();
+	bool InBounds(int i, int j);
+	float Average(int i, int j);
+	void CalcAllPatchBoundsY();
+	void CalcPatchBoundsY(UINT i, UINT j);
+	void BuildQuadPatchGeometry();
+private:
+	//PSO
+	static GraphicsPSO mPSO;
+
+	CD3DX12_CPU_DESCRIPTOR_HANDLE m_HeightMapDescriptor;
+	TerrainInitInfo mTerrainInfo;
 
 
+	UINT mNumPatchVertices = 0;
+	UINT mNumPatchQuadFaces = 0;
 
+	UINT mNumPatchVertRows = 0;
+	UINT mNumPatchVertCols = 0;
 
+	std::vector<XMFLOAT4X4> mWorld;
+
+	std::vector<XMFLOAT2> mPatchBoundsY;
+	std::vector<float> mHeightmap;
+
+	std::unique_ptr<MeshGeometry> mGeometries;
+
+	//srv index
+	int mBlendMapIndex;
+	int mHeightMapIndex;
 };
 
