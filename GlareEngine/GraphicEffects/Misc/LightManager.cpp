4c2c0f1ce2822d0bcd0a859b3ba558aac0e431eb
LihbGame <2252895330@qq.com>
2023-02-02 21:55:11 +0800

    update tiled light manager

--- GlareEngine/GraphicEffects/Misc/LightManager.cpp
+++ GlareEngine/GraphicEffects/Misc/LightManager.cpp
@@ -5,7 +5,7 @@
 #include "Engine/Camera.h"
 #include "Graphics/BufferManager.h"
 #include "Math/VectorMath.h"
-
+#include "Engine/EngineAdjust.h"
 
 //shaders
 #include "CompiledShaders/FillLightGrid_8_CS.h"
@@ -16,8 +16,81 @@
 using namespace GlareEngine::Math;
 using namespace GlareEngine;
 
+
+// Must keep in sync with HLSL
+struct LightData
+{
+	float PositionWS[3];
+	float PositionVS[3];
+	float RadiusSquare;
+	float Color[3];
+
+	uint32_t Type;
+	float ConeDir[3];
+	float ConeAngles[2];
+
+	float ShadowTextureMatrix[16];
+};
+
+enum { eMinLightGridDimension = 8 };
+
+namespace GlareEngine
+{
+	namespace Lighting
+	{
+		//light tile size
+		IntVar LightGridDimension("Rendering/Forward+/Light Grid Dimension", 16, eMinLightGridDimension, 32, 8);
+
+		//Light RootSignature
+		RootSignature m_FillLightRootSig;
+
+		//four tiled light types
+		ComputePSO m_FillLightGridCS_8(L"Fill Light Grid 8 CS");
+		ComputePSO m_FillLightGridCS_16(L"Fill Light Grid 16 CS");
+		ComputePSO m_FillLightGridCS_24(L"Fill Light Grid 24 CS");
+		ComputePSO m_FillLightGridCS_32(L"Fill Light Grid 32 CS");
+
+		LightData m_LightData[MaxLights];
+		StructuredBuffer m_LightBuffer;
+		ByteAddressBuffer m_LightGrid;
+
+		uint32_t m_FirstConeLight;
+		uint32_t m_FirstConeShadowedLight;
+
+		//shadow map size
+		enum { eShadowDimension = 512 };
+
+		ColorBuffer m_LightShadowArray;
+		ShadowBuffer m_LightShadowTempBuffer;
+		Matrix4 m_LightShadowMatrix[MaxLights];
+
+	}
+}
+
 void Lighting::InitializeResources(void)
 {
+	m_FillLightRootSig.Reset(3, 0);
+	m_FillLightRootSig[0].InitAsConstantBuffer(0);
+	m_FillLightRootSig[1].InitAsDescriptorRange(D3D12_DESCRIPTOR_RANGE_TYPE_SRV, 0, 2);
+	m_FillLightRootSig[2].InitAsDescriptorRange(D3D12_DESCRIPTOR_RANGE_TYPE_UAV, 0, 2);
+	m_FillLightRootSig.Finalize(L"FillLightRS");
+
+	m_FillLightGridCS_8.SetRootSignature(m_FillLightRootSig);
+	m_FillLightGridCS_8.SetComputeShader(g_pFillLightGrid_8_CS, sizeof(g_pFillLightGrid_8_CS));
+	m_FillLightGridCS_8.Finalize();
+
+	m_FillLightGridCS_16.SetRootSignature(m_FillLightRootSig);
+	m_FillLightGridCS_16.SetComputeShader(g_pFillLightGrid_16_CS, sizeof(g_pFillLightGrid_16_CS));
+	m_FillLightGridCS_16.Finalize();
+
+	m_FillLightGridCS_24.SetRootSignature(m_FillLightRootSig);
+	m_FillLightGridCS_24.SetComputeShader(g_pFillLightGrid_24_CS, sizeof(g_pFillLightGrid_24_CS));
+	m_FillLightGridCS_24.Finalize();
+
+	m_FillLightGridCS_32.SetRootSignature(m_FillLightRootSig);
+	m_FillLightGridCS_32.SetComputeShader(g_pFillLightGrid_32_CS, sizeof(g_pFillLightGrid_32_CS));
+	m_FillLightGridCS_32.Finalize();
+
 }
 
 void Lighting::CreateRandomLights(const Vector3 minBound, const Vector3 maxBound)
--- GlareEngine/GraphicEffects/Misc/LightManager.h
+++ GlareEngine/GraphicEffects/Misc/LightManager.h
@@ -3,7 +3,7 @@
 #include <cstdint>
 
 
-
+class Camera;
 
 namespace GlareEngine
 {
@@ -12,37 +12,35 @@ namespace GlareEngine
 		class Vector3;
 		class Matrix4;
 	}
-}
 
-namespace Lighting
-{
 	class StructuredBuffer;
 	class ByteAddressBuffer;
 	class ColorBuffer;
 	class ShadowBuffer;
 	class GraphicsContext;
 	class IntVar;
-	class Camera;
 
-	using namespace GlareEngine::Math;
+	namespace Lighting
+	{
+		using namespace GlareEngine::Math;
+		extern IntVar LightGridDim;
 
-	extern IntVar LightGridDim;
+		enum { MaxLights = 1024 };
 
-	enum { MaxLights = 1024 };
+		extern StructuredBuffer m_LightBuffer;
+		extern ByteAddressBuffer m_LightGrid;
 
-	extern StructuredBuffer m_LightBuffer;
-	extern ByteAddressBuffer m_LightGrid;
+		extern std::uint32_t m_FirstConeLight;
+		extern std::uint32_t m_FirstConeShadowedLight;
 
-	extern std::uint32_t m_FirstConeLight;
-	extern std::uint32_t m_FirstConeShadowedLight;
+		extern ColorBuffer m_LightShadowArray;
+		extern ShadowBuffer m_LightShadowTempBuffer;
+		extern Matrix4 m_LightShadowMatrix[MaxLights];
 
-	extern ColorBuffer m_LightShadowArray;
-	extern ShadowBuffer m_LightShadowTempBuffer;
-	extern Matrix4 m_LightShadowMatrix[MaxLights];
-
-	void InitializeResources(void);
-	void CreateRandomLights(const Vector3 minBound, const Vector3 maxBound);
-	void FillLightGrid(GraphicsContext& gfxContext, const Camera& camera);
-	void Shutdown(void);
+		void InitializeResources(void);
+		void CreateRandomLights(const Vector3 minBound, const Vector3 maxBound);
+		void FillLightGrid(GraphicsContext& gfxContext, const Camera& camera);
+		void Shutdown(void);
+	}
 }
 
