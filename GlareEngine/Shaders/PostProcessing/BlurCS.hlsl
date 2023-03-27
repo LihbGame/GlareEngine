89aca04ca2afdcbba699bba137d93aa9ed23f35a
LihbGame <2252895330@qq.com>
2023-03-27 23:18:11 +0800

    update Blur CS

--- GlareEngine/Shaders/PostProcessing/BlurCS.hlsl
+++ GlareEngine/Shaders/PostProcessing/BlurCS.hlsl
@@ -18,6 +18,40 @@ float3 BlurPixels(float3 a, float3 b, float3 c, float3 d, float3 e, float3 f, fl
     return Weights[0] * e + Weights[1] * (d + f) + Weights[2] * (c + g) + Weights[3] * (b + h) + Weights[4] * (a + i);
 }
 
+// 16x16 pixels to 8x8 pixels. Each uint is two color channels packed together
+groupshared uint CacheR[128];
+groupshared uint CacheG[128];
+groupshared uint CacheB[128];
+
+
+void StoreOnePixel(uint index, float3 pixel)
+{
+    CacheR[index] = asuint(pixel.r);
+    CacheG[index] = asuint(pixel.g);
+    CacheB[index] = asuint(pixel.b);
+}
+
+void LoadOnePixel(uint index, out float3 pixel)
+{
+    pixel = asfloat(uint3(CacheR[index], CacheG[index], CacheB[index]));
+}
+
+void StoreTwoPixels(uint index, float3 pixel1, float3 pixel2)
+{
+    CacheR[index] = f32tof16(pixel1.r) | f32tof16(pixel2.r) << 16;
+    CacheG[index] = f32tof16(pixel1.g) | f32tof16(pixel2.g) << 16;
+    CacheB[index] = f32tof16(pixel1.b) | f32tof16(pixel2.b) << 16;
+}
+
+void LoadTwoPixels(uint index, out float3 pixel1, out float3 pixel2)
+{
+    uint rr = CacheR[index];
+    uint gg = CacheG[index];
+    uint bb = CacheB[index];
+    pixel1 = float3(f16tof32(rr), f16tof32(gg), f16tof32(bb));
+    pixel2 = float3(f16tof32(rr >> 16), f16tof32(gg >> 16), f16tof32(bb >> 16));
+}
+
 
 [numthreads(8, 8, 1)]
 void main( uint3 DTid : SV_DispatchThreadID )
