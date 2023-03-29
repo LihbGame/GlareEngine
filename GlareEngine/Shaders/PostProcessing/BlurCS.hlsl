e01b31ec6b239e9872aeaa059cbfb0ddd49053df
LihbGame <2252895330@qq.com>
2023-03-29 23:22:48 +0800

    update Blur CS

--- GlareEngine/Shaders/PostProcessing/BlurCS.hlsl
+++ GlareEngine/Shaders/PostProcessing/BlurCS.hlsl
@@ -1,13 +1,12 @@
 #include "../Misc/CommonResource.hlsli"
 
-
 // Description: This is a compute shader that blurs the input texture.
 Texture2D<float3> InputBuffer : register(t0);
 RWTexture2D<float3> BlurResult : register(u0);
 
 cbuffer cb0 : register(b0)
 {
-    float2 g_inverseDimensions;
+    float2 g_InverseDimensions;
 }
 
 // The guassian blur weights (derived from Pascal's triangle)
@@ -52,6 +51,36 @@ void LoadTwoPixels(uint index, out float3 pixel1, out float3 pixel2)
     pixel2 = float3(f16tof32(rr >> 16), f16tof32(gg >> 16), f16tof32(bb >> 16));
 }
 
+// Blur two pixels horizontally.Reduces LDS reads and pixel unpacking.
+void BlurHorizontally(uint outIndex, uint leftMostIndex)
+{
+    float3 s0, s1, s2, s3, s4, s5, s6, s7, s8, s9;
+    LoadTwoPixels(leftMostIndex + 0, s0, s1);
+    LoadTwoPixels(leftMostIndex + 1, s2, s3);
+    LoadTwoPixels(leftMostIndex + 2, s4, s5);
+    LoadTwoPixels(leftMostIndex + 3, s6, s7);
+    LoadTwoPixels(leftMostIndex + 4, s8, s9);
+
+    StoreOnePixel(outIndex, BlurPixels(s0, s1, s2, s3, s4, s5, s6, s7, s8));
+    StoreOnePixel(outIndex + 1, BlurPixels(s1, s2, s3, s4, s5, s6, s7, s8, s9));
+}
+
+
+void BlurVertically(uint2 pixelCoord, uint topMostIndex)
+{
+    float3 s0, s1, s2, s3, s4, s5, s6, s7, s8;
+    LoadOnePixel(topMostIndex, s0);
+    LoadOnePixel(topMostIndex + 8, s1);
+    LoadOnePixel(topMostIndex + 16, s2);
+    LoadOnePixel(topMostIndex + 24, s3);
+    LoadOnePixel(topMostIndex + 32, s4);
+    LoadOnePixel(topMostIndex + 40, s5);
+    LoadOnePixel(topMostIndex + 48, s6);
+    LoadOnePixel(topMostIndex + 56, s7);
+    LoadOnePixel(topMostIndex + 64, s8);
+
+    BlurResult[pixelCoord] = BlurPixels(s0, s1, s2, s3, s4, s5, s6, s7, s8);
+}
 
 [numthreads(8, 8, 1)]
 void main( uint3 DTid : SV_DispatchThreadID )
