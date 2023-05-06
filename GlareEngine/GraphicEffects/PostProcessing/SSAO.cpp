23ab4b5ede48bd1aa53c26f7a331669c26f2b678
Ge Lhb <37478514+LihbGame@users.noreply.github.com>
2023-05-07 02:10:17 +0800

    update HDR Tonemapping

--- GlareEngine/GraphicEffects/PostProcessing/SSAO.cpp
+++ GlareEngine/GraphicEffects/PostProcessing/SSAO.cpp
@@ -1,15 +1,28 @@
 #include "SSAO.h"
+#include "Graphics/SamplerManager.h"
 
 namespace SSAO
 {
 	bool Enable = true;
 	bool AsyncCompute = false;
 	bool ComputeLinearZ = true;
+
+	RootSignature s_RootSignature;
 }
 
 
 void SSAO::Initialize(void)
 {
+	s_RootSignature.Reset(5, 2);
+	s_RootSignature.InitStaticSampler(0, SamplerLinearClampDesc);
+	s_RootSignature.InitStaticSampler(1, SamplerLinearBorderDesc);
+	s_RootSignature[0].InitAsConstants(0, 4);
+	s_RootSignature[1].InitAsConstantBuffer(1);
+	s_RootSignature[2].InitAsDescriptorRange(D3D12_DESCRIPTOR_RANGE_TYPE_UAV, 0, 5);
+	s_RootSignature[3].InitAsDescriptorRange(D3D12_DESCRIPTOR_RANGE_TYPE_SRV, 0, 5);
+	s_RootSignature[4].InitAsBufferSRV(5);
+	s_RootSignature.Finalize(L"SSAO");
+
 }
 
 void SSAO::Shutdown(void)
--- GlareEngine/Shaders/Present/PresentHDRPS.hlsl
+++ GlareEngine/Shaders/Present/PresentHDRPS.hlsl
@@ -6,6 +6,6 @@ Texture2D<float3> ColorTex : register(t0);
 
 float3 main(float4 position : SV_Position) : SV_Target0
 {
-    //50 is a scale for HDR color
-    return ApplyREC2084Curve(ColorTex[(int2)position.xy] / 50.0);
+    //10000 is a scale for HDR color
+    return ApplyREC2084Curve(ColorTex[(int2)position.xy] / 10000);
 }
