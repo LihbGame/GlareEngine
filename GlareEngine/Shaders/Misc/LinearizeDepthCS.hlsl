7b27960ff67863f4a49040817f27bfa05ef7c019
Ge Lhb <37478514+LihbGame@users.noreply.github.com>
2023-05-11 22:42:06 +0800

    add linear depth cs

--- GlareEngine/Shaders/Misc/LinearizeDepthCS.hlsl
+++ GlareEngine/Shaders/Misc/LinearizeDepthCS.hlsl
@@ -1,4 +1,13 @@
-[numthreads(1, 1, 1)]
-void main( uint3 DTid : SV_DispatchThreadID )
+RWTexture2D<float> LinearZ : register(u0);
+Texture2D<float> Depth : register(t0);
+
+cbuffer CB0 : register(b0)
 {
+    float ZMagic;                // (zFar - zNear) / zNear
+}
+
+[numthreads(16, 16, 1)]
+void main(uint3 DTid : SV_DispatchThreadID)
+{
+    LinearZ[DTid.xy] = 1.0 / (ZMagic * Depth[DTid.xy] + 1.0);
 }
