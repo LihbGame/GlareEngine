80f6f885272538ef36238e98fcacb14378812b6f
Ge Lhb <37478514+LihbGame@users.noreply.github.com>
2023-05-20 16:42:48 +0800

    Upgrade IMGUI MISC

--- Resource/UImisc/single_file/imgui_single_file.h
+++ Resource/UImisc/single_file/imgui_single_file.h
@@ -7,11 +7,23 @@
 //    #define IMGUI_IMPLEMENTATION
 // Before you include this file in *one* C++ file to create the implementation.
 // Using this in your project will leak the contents of imgui_internal.h and ImVec2 operators in this compilation unit.
+
+#ifdef IMGUI_IMPLEMENTATION
+#define IMGUI_DEFINE_MATH_OPERATORS
+#endif
+
 #include "../../imgui.h"
+#ifdef IMGUI_ENABLE_FREETYPE
+#include "../../misc/freetype/imgui_freetype.h"
+#endif
 
 #ifdef IMGUI_IMPLEMENTATION
 #include "../../imgui.cpp"
 #include "../../imgui_demo.cpp"
 #include "../../imgui_draw.cpp"
+#include "../../imgui_tables.cpp"
 #include "../../imgui_widgets.cpp"
+#ifdef IMGUI_ENABLE_FREETYPE
+#include "../../misc/freetype/imgui_freetype.cpp"
+#endif
 #endif
