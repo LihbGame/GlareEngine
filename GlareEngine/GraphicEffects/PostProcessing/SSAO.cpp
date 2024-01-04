8165a83d385f201a23e0b88873aeebbaa9d4705b
LihbGame <LihbGame@gmail.com>
2024-01-04 23:35:38 +0800

    [SSAO] Add temporal jitter for ssao

--- GlareEngine/GraphicEffects/PostProcessing/SSAO.cpp
+++ GlareEngine/GraphicEffects/PostProcessing/SSAO.cpp
@@ -3,6 +3,7 @@
 #include "PostProcessing/PostProcessing.h"
 #include "Engine/EngineProfiling.h"
 #include "EngineGUI.h"
+#include "TemporalAA.h"
 
 //shader
 #include "CompiledShaders/SsaoCS.h"
@@ -31,6 +32,7 @@ namespace SSAO
 		float			Range;
 		float			Power;
 		XMFLOAT2		InverseDimensions;
+		XMFLOAT2		Jitter;
 		int				SampleCount;
 	};
 }
@@ -82,7 +84,7 @@ void SSAO::Render(GraphicsContext& Context, MainConstants& RenderData)
 			Matrix4(RenderData.Proj),
 			Matrix4(RenderData.InvProj),
 			RenderData.FarZ,RenderData.NearZ,
-			SsaoRange,SsaoPower,XMFLOAT2(1.0f / g_SSAOFullScreen.GetWidth(),1.0f / g_SSAOFullScreen.GetHeight()),SsaoSampleCount };
+			SsaoRange,SsaoPower,XMFLOAT2(1.0f / g_SSAOFullScreen.GetWidth(),1.0f / g_SSAOFullScreen.GetHeight()),TemporalAA::GetJitterOffset(), SsaoSampleCount };
 
 		computeContext.SetDynamicConstantBufferView(3, sizeof(SSAORenderData), &renderData);
 
