80a12d0ac927b1798c3d108138c485f135f9ef76
LihbGame <LihbGame@gmail.com>
2024-02-06 20:56:41 +0800

    update mesh render pass

---
+++ GlareEngine/EngineCore/Model/MeshRenderPass.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:670f60819077092ffad89b6179a4c76c76e45c24f92b1020fb00d808d6a29e63
+size 8976
---
+++ GlareEngine/EngineCore/Model/MeshRenderPass.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6e7a538753b60730110c043e694dcbc17e5dcd2466d4cabbf872e5d3e3666f64
+size 2585
