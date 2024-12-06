742dfb05f395ee0b4123911b9cf13e220cff6221
Ge Lhb <37478514+LihbGame@users.noreply.github.com>
2024-12-07 01:49:03 +0800

    [Area light] update Rotation matrix for area light mesh

--- GlareEngine/GraphicEffects/Misc/LightManager.cpp
+++ GlareEngine/GraphicEffects/Misc/LightManager.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:672720919b84bcc887406216db9f9d15ead2e21da812a8ab5572740cc4d568d0
-size 24564
+oid sha256:7bdd237c1aa14743a5249a9c32e1bcd4d7fdee2db9c5a65ef3672d929f9312f6
+size 24668
