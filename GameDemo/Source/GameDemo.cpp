f5ae51123bdb214e3fd167c45c1e7bcc8e30da6d
Ge Lhb <37478514+LihbGame@users.noreply.github.com>
2024-12-11 23:22:53 +0800

    FIX instance model bug

--- GameDemo/Source/GameDemo.cpp
+++ GameDemo/Source/GameDemo.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:592f6c4024bb2be798f5e9d7f8d887f48dce64c97a5741b2c0a5168eaeeaf4d1
-size 18829
+oid sha256:65548d4f71a4879aaeb3cbfd2e371c1a6ef4445b2d9e42cdd60050e8010bb777
+size 18808
