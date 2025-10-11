f86849183d06fb8154b8d0e6d260fe980b0b4bcf
Ge Lhb <37478514+LihbGame@users.noreply.github.com>
2025-10-11 23:27:33 +0800

    update CMake

--- GlareEngine/EngineCore/Graphics/DLSS.cpp
+++ GlareEngine/EngineCore/Graphics/DLSS.cpp
@@ -1,1 +1,28 @@
 #include "DLSS.h"
+
+
+DLSS* DLSS::m_pDLSSInstance = nullptr;
+
+
+DLSS* DLSS::GetInstance()
+{
+	if (m_pDLSSInstance == nullptr)
+	{
+		m_pDLSSInstance = new DLSS();
+	}
+	return m_pDLSSInstance;
+}
+
+void DLSS::Shutdown()
+{
+	if (m_pDLSSInstance)
+	{
+		delete m_pDLSSInstance;
+		m_pDLSSInstance = nullptr;
+	}
+}
+
+DLSS::~DLSS()
+{
+
+}
--- GlareEngine/EngineCore/Graphics/DLSS.h
+++ GlareEngine/EngineCore/Graphics/DLSS.h
@@ -1,5 +1,18 @@
 #pragma once
+
 class DLSS
 {
+public:
+	static DLSS* GetInstance();
+	static void Shutdown();
+
+
+private:
+	DLSS() {}
+	~DLSS();
+
+private:
+	static DLSS* m_pDLSSInstance;
+
 };
 
